59439ec2807370b133f06fbde02cc550e9ed10ab
Hiroshi Nagata <hirosn@live.com>
2014-09-18 00:55:49 -0700

    Add basic HTML/CSS/JavaScript(jQuery) pattern.

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function()
+{
+   $('div').mouseenter(function()
+   {
+       $(this).animate(
+       {
+           height: '+=10px'
+       });
+   });
+
+   $('div').mouseleave(function()
+   {
+       $(this).animate(
+       {
+           height: '-=10px'
+       }); 
+   });
+
+   $('div').click(function()
+   {
+       $(this).toggle(1000);
+   }); 
+});
