95bc0023d6889e858c51b9d1d25c0bbdf3d13cbe
Hiroshi Nagata <hirosn@live.com>
2014-09-19 00:48:33 -0700

    Changes in spaces only.

--- helloworld/script.js
+++ helloworld/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-   $('div').mouseenter(function() {
-       $(this).animate({height: '+=10px'});
-   });
+	$('div').mouseenter(function() {
+		$(this).animate({height: '+=10px'});
+	});
 
-   $('div').mouseleave(function() {
-       $(this).animate({height: '-=10px'}); 
-   });
+	$('div').mouseleave(function() {
+		$(this).animate({height: '-=10px'});
+	});
 
-   $('div').click(function() {
-       $(this).toggle(1000);
-   }); 
+	$('div').click(function() {
+		$(this).toggle(1000);
+	}); 
 });
