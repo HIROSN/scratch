15bfedc6039ae3b237ab316f20d0ebbf08806963
Hiroshi Nagata <hirosn@live.com>
2015-01-05 12:54:33 -0800

    Add input

--- helloworld/script.js
+++ helloworld/script.js
@@ -1,16 +1,50 @@
 $(function() {
-  for (var i = 0; i < 10; i++) {
-    $('.myContent').append($('<div class="App_v2"></div>'));
+  var $myContent = $('.myContent');
+  var $myColumns = $('#myColumns');
+  var $showColumn = $('#showColumn');
+  var $numContents = $('#numContents');
+
+  var showHideColumn = function() {
+    if ($showColumn.is(':checked')) {
+      $myColumns.removeClass('hidden');
+    }
+    else {
+      $myColumns.addClass('hidden');
+    }
   }
 
-  $('.App_v2')
-  .mouseenter(function() {
+  var showContents = function() {
+    $('.myContent div').remove();
+
+    for (var i = 0; i < +$numContents.val(); i++) {
+      $myContent.append($('<div class="App_v2"></div>'));
+    }
+  }
+
+  $showColumn.on('click', function() {
+    showHideColumn();
+  });
+
+  $numContents.on('keyup', function(event) {
+    if (13 === event.which) {
+      $(this).blur();
+    }
+  });
+
+  $numContents.on('blur', function() {
+    showContents();
+  });
+
+  $myContent.on('mouseenter', '.App_v2', function() {
     $(this).animate({width: '+=10px'});
   })
-  .mouseleave(function() {
+  .on('mouseleave', '.App_v2', function() {
     $(this).animate({width: '-=10px'});
   })
-  .click(function() {
+  .on('click', '.App_v2', function() {
     $(this).toggle(1000);
   });
+
+  showHideColumn();
+  showContents();
 });
