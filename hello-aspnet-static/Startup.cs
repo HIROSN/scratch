c94af97edb14e1796443787da5636eae9c58c6c6
Hiroshi Nagata <hirosn@live.com>
2015-10-18 23:46:51 -0700

    serve static files

--- hello-aspnet-static/Startup.cs
+++ hello-aspnet-static/Startup.cs
@@ -17,10 +17,8 @@ namespace hello_aspnet_static
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
