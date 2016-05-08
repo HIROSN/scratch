a598f474b400ec8580b706f9b188a61de5c49669
Hiroshi Nagata <hirosn@live.com>
2016-05-08 13:03:21 -0700

    use subset of mtcars

--- hello-R/Base.R
+++ hello-R/Base.R
@@ -1,16 +1,17 @@
-df <- data.frame(
-  Name = c("a", "b", "c"),
-  Value = c(1, 2, 3))
+# df <- data.frame(
+#   Name = c("a", "b", "c"),
+#   Value = c(1, 2, 3))
 #df
 #plot(df)
 #plot(df$Name, df$Value)
-#plot(
-  #x = df$Name,
-  #y = df$Value)
+# plot(
+#   x = df$Name,
+#   y = df$Value)
+merc <- subset(mtcars, grepl("^Merc.*", row.names(mtcars)) & mtcars$cyl >= 8)
 barplot(
-  names = df$Name,
-  height = df$Value,
+  names = row.names(merc),
+  height = merc$mpg,
   col = "skyblue",
-  main = "Hello World",
-  xlab = "Name",
-  ylab = "Value")
+  main = "Mercedes",
+  xlab = "Type",
+  ylab = "MPG")
